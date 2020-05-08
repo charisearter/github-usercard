111f664a601b31125bd3e5cec7002d80994192d8
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-05-07 20:12:04 -0500

    finished reset may work hopefully

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -20,7 +20,6 @@
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
     data in order to use it to build your component function
-
     Skip to STEP 3.
 */
 
@@ -37,12 +36,7 @@ axios.get(`https://api.github.com/users/${gitHubName}`)
   let theUser = response.data;
     cardEntry.appendChild(userCardMaker(theUser));
   })
-.catch(error => {
-  alert('There is no user by that name.')
-})
-.finally(() => {
-  console.log('Done.')
-})
+
 
 }//ends get Card function
 
@@ -54,7 +48,6 @@ axios.get(`https://api.github.com/users/${gitHubName}`)
     manually find some other users' github handles, or use the list found at the
     bottom of the page. Get at least 5 different Github usernames and add them as
     Individual strings to the friendsArray below.
-
     Using that array, iterate over it, requesting data for each user, creating a new card for each
     user, and adding that card to the DOM.
 */
@@ -68,12 +61,11 @@ followersArray.forEach(item => {
     cardEntry.appendChild(userCardMaker(followerInfo));
   })
 });
-console.log(followersArray);
+git
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
-
     <div class="card">
       <img src={image url of user} />
       <div class="card-info">
